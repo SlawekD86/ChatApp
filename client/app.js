322579ccbfea71ac1cada619498d554037ab5e4f
Slawek Derabin <slawomirderabin@gmail.com>
2023-06-19 13:39:20 +0200

    add info

--- client/app.js
+++ client/app.js
@@ -1,41 +1,48 @@
+const socket = io();
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
-
 let userName = '';
-
 loginForm.addEventListener('submit', function login(event) {
-  event.preventDefault();
+    event.preventDefault();
+  
+    if (userNameInput.value.length > 0) {
+      userName = userNameInput.value;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show');
+      socket.emit('user', { author: userName })
+      console.log('logged in as:', userName);
 
-  if (userNameInput.value.length > 0) {
-    userName = userNameInput.value;
-    loginForm.classList.remove('show');
-    messagesSection.classList.add('show');
-    console.log('logged in as:', userName);
-  } else {
-    alert('You must choose username :)');
-  }
-});
-
+      // Usuwanie istniejących wiadomości z listy
+      while (messagesList.firstChild) {
+        messagesList.removeChild(messagesList.firstChild);
+      }
+    } else {
+      alert('You must choose username :)');
+    }
+  });
 addMessageForm.addEventListener('submit', function sendMessage(event) {
   event.preventDefault();
-
   if (messageContentInput.value.length > 0) {
     addMessage(userName, messageContentInput.value);
+    socket.emit('message', { author: userName, content: messageContentInput.value })
     messageContentInput.value = '';
   } else {
     alert('There is no message here');
   }
 });
-
 function addMessage(author, content) {
   const message = document.createElement('li');
   message.classList.add('message');
   message.classList.add('message--received');
-  if (author === userName) message.classList.add('message--self');
+  if (author === userName) {
+    message.classList.add('message--self');
+  } else if (author === 'ChatBot') {
+    message.classList.add('message--bot')  
+  }
   message.innerHTML = `
       <h3 class="message__author">${userName === author ? 'You' : author}</h3>
       <div class="message__content">
@@ -44,3 +51,4 @@ function addMessage(author, content) {
     `;
   messagesList.appendChild(message);
 }
+socket.on('message', ({ author, content }) => addMessage(author, content))
--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const socket = require('socket.io');
 const messages = [];
-
+let users = [];
 app.use(express.static(path.join(__dirname, '/client')));
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+const server = app.listen(8000, () => {
+    console.log('Server is running on Port:', 8000);
+});
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('I\'ve added a listener on message event \n');
+
+    socket.on('user', (user) => {
+        users.push({ user: user.author, userId: socket.id })
+        console.log('Oh, I\'ve got new user', users)
+        socket.broadcast.emit('message', { author: 'Chat Bot', content: user.author + ` has joined the conversation` });
+    })
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        const disconnectedUser = users.find((user) => user.userId === socket.id);
+        if (disconnectedUser) {
+            socket.broadcast.emit('message', { author: 'Chat Bot', content: disconnectedUser.user + ' has left the conversation' });
+            console.log('Oh, socket ' + socket.id + ' has left');
+        }
+    });
 });
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'not found' });
-});
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
-});
+    res.status(404).json({ message: 'not found' });
+})
